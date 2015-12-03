81882561f51fc0971f2cda9cdfc1e70b52c0a938
luispscarvalho <luispscarvalho@gmail.com>
2015-12-02 22:14:30 -0300

    Nao utilize acentuacao e caracteres especiais

--- vigilancia/javaProject/driverVigilancia/src/br/edu/ifba/embarcados/drivervigilancia/asincexec/IListenerAcelerometro.java
+++ vigilancia/javaProject/driverVigilancia/src/br/edu/ifba/embarcados/drivervigilancia/asincexec/IListenerAcelerometro.java
@@ -3,6 +3,8 @@ package br.edu.ifba.embarcados.drivervigilancia.asincexec;
 public interface IListenerAcelerometro {
 	
 	public void notificarMovimento(int x, int y, int z);
+	
+	// FIXME nunca utilize acentuação ou caracteres especiais ao nomear funcoes
 	public void notificarVibração();
 
 }
